4b72658f3f268f99ccf4462b50af2967c3e5d5a9
Thongculan <87519946+Thongculan@users.noreply.github.com>
2021-07-16 15:38:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    introTitle: 'Hello NgocDieu Cute!',
+    introDesc: `Anh có câu hỏi này
+    muốn hỏi NgocDieu nè !! 
+    Trả Lời thật nhaaaa ❤❤❤❤❤`,
+    btnIntro: 'Dạaaaaa',
+    title: 'Anh Thích NgocDieu lắm luôn đóoo 😙',
+    desc: 'NgocDieu có thích anh honggg ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnNo: 'Không ! anh điên à ?',
+    question:'Ủa v lí do tại sao mà thích anhhh dạ ? <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: ' Tại vì thích là thích thôiiii <33333333',
+    mess: 'Ngại quá đi thoiii 🥰. tym cho NgocDieu 😘😘',
+    messDesc: 'Vậy mình đi chơi nhaaaa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/minhthong4920' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
